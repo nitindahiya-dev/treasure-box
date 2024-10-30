cb191c8fd8a90ccfce6a2d12fabb9f7cde910472
nitindahiya-dev <nitindahiya00000@gmail.com>
2024-10-30 19:19:44 +0530

    frontend done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,37 @@
 import Navbar from '@/components/Navbar'
 import React from 'react'
 import { Vortex } from "@/components/ui/vortex";
-
+import Footer from '@/components/Footer';
 
 const page = () => {
 
   return (
-      <Vortex
-        backgroundColor="black"
-        // rangeY={800}
-        // particleCount={500}
-        // baseHue={120}
-        className="w-[calc(100%-4rem)] mx-auto rounded-md  h-screen overflow-hidden flex items-center flex-col justify-center px-2 md:px-10  py-4"
-      >
-    <div className="">
-        <Navbar />
-    </div>
-      </Vortex>
+    <Vortex
+      backgroundColor="black"
+      className="mx-auto rounded-md  min-h-screen overflow-hidden flex items-center flex-col justify-between px-2 md:px-10  py-6"
+    >
+      <Navbar />
+      <div className="bg-white shadow-lg rounded-xl md:w-[22vw] ">
+        <div className="h-[40vh] pt-5 rounded-xl relative">
+          <iframe
+            src="https://giphy.com/embed/prhu1VNvjvJQNaJnEc"
+            title="Giphy Embed"
+            width="100%"
+            height="100%"
+            className="pointer-events-none"
+          ></iframe>
+          <div className="absolute inset-0"></div>
+        </div>
+        <div className="flex flex-col gap-1 p-5">
+          <p className='font-bold text-md text-black'>Mint the master key to win a Prize Poll of $30,000</p>
+          <p className='text-md text-gray-700'>18 hrs 19 min left vdr6e8v68...1y6etv1 to win</p>
+          <button className='w-full font-bold text-md h-8 text-white bg-black rounded-md'>Mint for 0.32 SOL</button>
+        </div>
+      </div>
+
+      <Footer />
+
+    </Vortex>
   )
 }
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,58 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex justify-between flex-wrap items-center md:w-[40vw] mx-auto h-16 rounded-full px-5 backdrop-blur-lg bg-white/10 shadow-lg'>
+      <div className="flex items-center gap-2">
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="circle-info"
+          stroke="white"
+          className="svg-inline--fa fa-circle-info h-[18px]"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path
+            fill="white"
+            d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336l24 0 0-64-24 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l48 0c13.3 0 24 10.7 24 24l0 88 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-80 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"
+          ></path>
+        </svg>
+        <span className='text-white font-bold text-xl'>About</span>
+      </div>
+      <Link target='_blank' href={"https://solana-tools-xi.vercel.app/"}>
+        <div className="flex items-center gap-2">
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fas"
+            data-icon="puzzle-piece"
+            className="svg-inline--fa fa-puzzle-piece h-[18px]"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <path
+              fill="white"
+              d="M192 104.8c0-9.2-5.8-17.3-13.2-22.8C167.2 73.3 160 61.3 160 48c0-26.5 28.7-48 64-48s64 21.5 64 48c0 13.3-7.2 25.3-18.8 34c-7.4 5.5-13.2 13.6-13.2 22.8c0 12.8 10.4 23.2 23.2 23.2l56.8 0c26.5 0 48 21.5 48 48l0 56.8c0 12.8 10.4 23.2 23.2 23.2c9.2 0 17.3-5.8 22.8-13.2c8.7-11.6 20.7-18.8 34-18.8c26.5 0 48 28.7 48 64s-21.5 64-48 64c-13.3 0-25.3-7.2-34-18.8c-5.5-7.4-13.6-13.2-22.8-13.2c-12.8 0-23.2 10.4-23.2 23.2L384 464c0 26.5-21.5 48-48 48l-56.8 0c-12.8 0-23.2-10.4-23.2-23.2c0-9.2 5.8-17.3 13.2-22.8c11.6-8.7 18.8-20.7 18.8-34c0-26.5-28.7-48-64-48s-64 21.5-64 48c0 13.3 7.2 25.3 18.8 34c7.4 5.5 13.2 13.6 13.2 22.8c0 12.8-10.4 23.2-23.2 23.2L48 512c-26.5 0-48-21.5-48-48L0 343.2C0 330.4 10.4 320 23.2 320c9.2 0 17.3 5.8 22.8 13.2C54.7 344.8 66.7 352 80 352c26.5 0 48-28.7 48-64s-21.5-64-48-64c-13.3 0-25.3 7.2-34 18.8C40.5 250.2 32.4 256 23.2 256C10.4 256 0 245.6 0 232.8L0 176c0-26.5 21.5-48 48-48l120.8 0c12.8 0 23.2-10.4 23.2-23.2z"
+            ></path></svg>
+
+          <span className='text-white font-bold text-xl'>See More Fun Things</span>
+        </div>
+      </Link>
+
+      <Link target='_blank' href={"https://github.com/nitindahiya-dev/treasure-box"}>
+        <div className="flex items-center gap-2">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="white" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" /><path d="M9 18c-4.51 2-5-2-7-2" /></svg>
+
+          <span className='text-white font-bold text-xl'>Star on github</span>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default Footer
